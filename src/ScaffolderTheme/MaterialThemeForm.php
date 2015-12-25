4521d0da2fa2a7b94ce5aa722595a7850d1bc031
Konstantin Mirin <konstantin.mirin@iqria.com>
2015-12-25 21:28:32 +0100

    Form generator updates

--- src/ScaffolderTheme/MaterialThemeForm.php
+++ src/ScaffolderTheme/MaterialThemeForm.php
@@ -6,15 +6,42 @@ use Collective\Html\FormBuilder;
 
 class MaterialThemeForm extends FormBuilder
 {
-    public function text($name, $value = null, $options = [])
-    {
-        return $this->commonInputs('text', $name, $value, $options);
+    public function __call($method, $arguments) {
+        if (!method_exists($this, $method)) {
+            return false;
+        }
+        $input = call_user_func_array([$this, $method], $arguments);
+        if (!in_array($method, ['text', 'password', 'number', 'email', 'tel', 'date', 'datetime'])) { //we might be trying to wrap some other method that is not field
+            return $input;
+        }
+        //assuming that $name is always the first argument and $options is always last one
+        return $this->wrapField($input, $arguments[0], $arguments[sizeof($arguments) - 1]);
     }
 
-    public function number($name, $value = null, $options = [])
+    protected function wrapField($field, $name, $options)
     {
-        return $this->commonInputs('number', $name, $value, $options);
+        $labelText = isset($options['label']) ? $options['label'] : null;
+        return sprintf('
+        <div class="input-field col l6 s8 offset-l3 offset-s2">
+            %s
+            %s
+        </div>', $field, parent::label($name, $labelText, []));
     }
+
+    /* public function text($name, $value = null, $options = [])
+     {
+         return $this->commonInputs('text', $name, $value, $options);
+     }
+
+     public function password($name, $options = [])
+     {
+         return $this->commonInputs('password', $name, '', $options);
+     }
+
+     public function number($name, $value = null, $options = [])
+     {
+         return $this->commonInputs('number', $name, $value, $options);
+     }*/
 
     public function textarea($name, $value = null, $options = [])
     {
@@ -55,12 +82,13 @@ class MaterialThemeForm extends FormBuilder
         </div>', parent::radio($name, $value, $checked, $options), parent::label($options['id'], $value, []));
     }
 
-    private function commonInputs($type, $name, $value = null, $options = [])
+    /*private function commonInputs($type, $name, $value = null, $options = [])
     {
+        $labelText = isset($options['label']) ? $options['label'] : null;
         return sprintf('
         <div class="input-field col l6 s8 offset-l3 offset-s2">
             %s
             %s
-        </div>', parent::input($type, $name, $value, $options), parent::label($name, 'test test', []));
-    }
+        </div>', parent::input($type, $name, $value, $options), parent::label($name, $labelText, []));
+    }*/
 }
