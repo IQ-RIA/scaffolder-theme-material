4eb698852c725f81bd5fd02b7fcff877aeb427ee
Konstantin Mirin <konstantin.mirin@iqria.com>
2015-12-26 13:08:26 +0100

    Reworked the approach for materializing the form controls. Now you need to call materialText instead of just text. This allows me to wrap your call into a decorator function and still allow you to customize your own logic by defining materialControl right in the ThemeForm class

--- src/ScaffolderTheme/MaterialThemeForm.php
+++ src/ScaffolderTheme/MaterialThemeForm.php
@@ -6,89 +6,121 @@ use Collective\Html\FormBuilder;
 
 class MaterialThemeForm extends FormBuilder
 {
-    public function __call($method, $arguments) {
+    protected $prefix = 'material';
+
+    public function __call($method, $arguments)
+    {
+        //convert materialText -> text and materialSomeControl -> someControl
+        $method = str_replace($this->prefix, '', $method);
+        $method = strtolower($method[0]) . substr($method, 1);
         if (!method_exists($this, $method)) {
             return false;
         }
+        $arguments = $this->adjustArguments($method, $arguments);
         $input = call_user_func_array([$this, $method], $arguments);
-        if (!in_array($method, ['text', 'password', 'number', 'email', 'tel', 'date', 'datetime'])) { //we might be trying to wrap some other method that is not field
-            return $input;
-        }
         //assuming that $name is always the first argument and $options is always last one
         return $this->wrapField($input, $arguments[0], $arguments[sizeof($arguments) - 1]);
     }
 
-    protected function wrapField($field, $name, $options)
+    protected function adjustArguments($method, $arguments)
     {
-        $labelText = isset($options['label']) ? $options['label'] : null;
-        return sprintf('
-        <div class="input-field col l6 s8 offset-l3 offset-s2">
-            %s
-            %s
-        </div>', $field, parent::label($name, $labelText, []));
+        switch ($method) {
+            case 'textarea':
+                //injecting class=materialize-textarea
+                $className = $arguments[sizeof($arguments) - 1]['class'];
+                if ($className) {
+                    if (strpos($className, 'materialize-textarea') === false) {
+                        $className .= ' materialize-textarea';
+                    }
+                } else {
+                    $className = 'materialize-textarea';
+                }
+                $options['class'] = $className;
+                $arguments[sizeof($arguments) - 1]['class'] = $className;
+                break;
+        }
+        return $arguments;
     }
 
-    /* public function text($name, $value = null, $options = [])
-     {
-         return $this->commonInputs('text', $name, $value, $options);
-     }
-
-     public function password($name, $options = [])
-     {
-         return $this->commonInputs('password', $name, '', $options);
-     }
-
-     public function number($name, $value = null, $options = [])
-     {
-         return $this->commonInputs('number', $name, $value, $options);
-     }*/
-
-    public function textarea($name, $value = null, $options = [])
+    protected function wrapField($field, $labelName, $fieldOptions)
     {
+        $labelText = isset($fieldOptions['label']) ? $fieldOptions['label'] : null;
         return sprintf('
-        <div class="input-field col l6 s8 offset-l3 offset-s2">
+        <div class="input-field col l3 m4 s8 offset-l2 offset-m1 offset-s2">
             %s
             %s
-        </div>', parent::textarea($name, $value, ['class' => 'materialize-textarea']), parent::label($name, null, []));
-    }
-
-    public function select($name, $list = [], $selected = null, $options = [])
-    {
-        return sprintf('
-        <div class="input-field col l6 s8 offset-l3 offset-s2">
-            %s
-            %s
-        </div>', parent::select($name, $list, $selected, $options), parent::label($name, null, []));
+        </div>', $field, parent::label($labelName, $labelText, []));
     }
 
     public function checkbox($name, $value = 1, $checked = null, $options = [])
     {
-        $options['id'] = $name;
-
-        return sprintf('
-        <div class="input-field col l6 s8 offset-l3 offset-s2">
-            %s
-            %s
-            %s
-        </div>', parent::hidden($name, 0), parent::checkbox($name, $value, $checked, $options), parent::label($name, null, []));
+        $options['id'] = isset($options['id']) ? $options['id'] : $name;
+        return parent::hidden($name, 0) . call_user_func_array('parent::' . __FUNCTION__, [$name, $value, $checked = null, $options]);
     }
 
-    public function radio($name, $value = null, $checked = null, $options = [])
+    public function materialRadio($name, $value = null, $checked = null, $options = [])
     {
-        return sprintf('
-        <div class="input-field col l6 s8 offset-l3 offset-s2">
-            %s
-            %s
-        </div>', parent::radio($name, $value, $checked, $options), parent::label($options['id'], $value, []));
+        $input = call_user_func_array('parent::' . __FUNCTION__, func_get_args());
+        $options['label'] = isset($options['label']) ? $options['label'] : $value;
+        return $this->wrapField($input, $options['id'] ?: $name, $options);
     }
 
-    /*private function commonInputs($type, $name, $value = null, $options = [])
+    public function usState($name = 'state', $selected = null, $options = [])
     {
-        $labelText = isset($options['label']) ? $options['label'] : null;
-        return sprintf('
-        <div class="input-field col l6 s8 offset-l3 offset-s2">
-            %s
-            %s
-        </div>', parent::input($type, $name, $value, $options), parent::label($name, $labelText, []));
-    }*/
+        $states = [
+            'AL'=>'Alabama',
+            'AK'=>'Alaska',
+            'AZ'=>'Arizona',
+            'AR'=>'Arkansas',
+            'CA'=>'California',
+            'CO'=>'Colorado',
+            'CT'=>'Connecticut',
+            'DE'=>'Delaware',
+            'DC'=>'District of Columbia',
+            'FL'=>'Florida',
+            'GA'=>'Georgia',
+            'HI'=>'Hawaii',
+            'ID'=>'Idaho',
+            'IL'=>'Illinois',
+            'IN'=>'Indiana',
+            'IA'=>'Iowa',
+            'KS'=>'Kansas',
+            'KY'=>'Kentucky',
+            'LA'=>'Louisiana',
+            'ME'=>'Maine',
+            'MD'=>'Maryland',
+            'MA'=>'Massachusetts',
+            'MI'=>'Michigan',
+            'MN'=>'Minnesota',
+            'MS'=>'Mississippi',
+            'MO'=>'Missouri',
+            'MT'=>'Montana',
+            'NE'=>'Nebraska',
+            'NV'=>'Nevada',
+            'NH'=>'New Hampshire',
+            'NJ'=>'New Jersey',
+            'NM'=>'New Mexico',
+            'NY'=>'New York',
+            'NC'=>'North Carolina',
+            'ND'=>'North Dakota',
+            'OH'=>'Ohio',
+            'OK'=>'Oklahoma',
+            'OR'=>'Oregon',
+            'PA'=>'Pennsylvania',
+            'RI'=>'Rhode Island',
+            'SC'=>'South Carolina',
+            'SD'=>'South Dakota',
+            'TN'=>'Tennessee',
+            'TX'=>'Texas',
+            'UT'=>'Utah',
+            'VT'=>'Vermont',
+            'VA'=>'Virginia',
+            'WA'=>'Washington',
+            'WV'=>'West Virginia',
+            'WI'=>'Wisconsin',
+            'WY'=>'Wyoming',
+        ];
+        return $this->select($name, $states, $selected, $options);
+    }
+
 }
